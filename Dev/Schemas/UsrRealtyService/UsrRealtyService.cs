6c7978f36beca55f05d197cc4e7c953d60f529d1
akshay39 <akshay@geekyworks.com>
2023-04-05 16:50:19 +0530

    screma changes

--- Dev/Schemas/UsrRealtyService/UsrRealtyService.cs
+++ Dev/Schemas/UsrRealtyService/UsrRealtyService.cs
@@ -1,47 +1,69 @@
 namespace Terrasoft.Configuration 
+
 { 
-    using System.ServiceModel; 
-    using System.ServiceModel.Web;
-	using System.ServiceModel.Activation;
+
+	using System.ServiceModel; 
+
+	using System.ServiceModel.Web; 
+
+	using System.ServiceModel.Activation; 
+
 	using Terrasoft.Core.DB; 
+
 	using Terrasoft.Web.Common; 
-	using System;
-	using System.Web.SessionState;
-	
-	[ServiceContract]
+
+	using System; 
+
+	using System.Web.SessionState; 
+
+	[ServiceContract] 
+
 	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)] 
 
 	public class RealtyService : BaseService, IReadOnlySessionState 
-	{ 
-		[OperationContract]
-		[WebInvoke(Method = "POST", BodyStyle = WebMessageBodyStyle.Wrapped,
-			RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
-		public decimal GetTotalAmountByTypeId(string realtyTypeId, string realtyOfferTypeId,
-			string entityName)
-		{
-			if (string.IsNullOrEmpty(realtyTypeId) || string.IsNullOrEmpty(realtyOfferTypeId)) 
-			{ 
-				return -1; 
-			} 
-			decimal result = 0;
-			Select select = new Select(UserConnection) 
-				.Column(Func.Sum("UsrPrice"))
-				.From("UsrRealty")
-				.Where("UsrTypeId").IsEqual(Column.Parameter(new Guid(realtyTypeId)))
-				.And("UsrOfferTypeId").IsEqual(Column.Parameter(new Guid(realtyOfferTypeId)))
-				as Select;
-			result = select.ExecuteScalar<decimal>(); 
-			return result; 
 
-		}
+	{ 
+
 		[OperationContract] 
-		[WebInvoke(Method = "GET", BodyStyle = WebMessageBodyStyle.Wrapped,
+
+		[WebInvoke(Method = "POST", BodyStyle = WebMessageBodyStyle.Wrapped, 
+
 			RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)] 
-		public string GetExample() 
+
+  
+
+		public decimal GetTotalAmountByTypeId(string realtyTypeId, string realtyOfferTypeId) 
 
 		{ 
-			return "OK!";
+
+			if (string.IsNullOrEmpty(realtyTypeId) || string.IsNullOrEmpty(realtyOfferTypeId)) 
+
+			{ 
+
+				return -1; 
+
+			} 
+
+			decimal result = 0; 
+
+			Select select = new Select(UserConnection) 
+
+				.Column(Func.Sum("UsrPrice")) 
+
+				.From("UsrRealty") 
+
+				.Where("UsrTypeId").IsEqual(Column.Parameter(new Guid(realtyTypeId))) 
+
+				.And("UsrOfferTypeId").IsEqual(Column.Parameter(new Guid(realtyOfferTypeId))) 
+
+				as Select; 
+
+			result = select.ExecuteScalar<decimal>(); 
+
+			return result; 
 
 		} 
-	}
-}
+
+	} 
+
+} 
